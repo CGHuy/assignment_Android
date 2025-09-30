f1230168585c7d3771eda35fd0fab0f31951c63f
Cao Giang Huy <164297800+CGHuy@users.noreply.github.com>
2025-09-30 13:18:13 +0700

    lab00

---
+++ MainActivity.java
@@ -0,0 +1,14 @@
+package com.app.lab_00;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.bai1);
+    }
+}
